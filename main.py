c92422c3b3be7fd14d6bede44f83af3a542fae1a
Vladimir Kostousov <vovakost@yandex.ru>
2023-05-27 11:21:59 +0300

    task-2

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+
+class Lecture(Mentor):
+    '''класс лекторов'''
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -7,13 +20,18 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecture(self, lecture, course, grade):
+        if isinstance(lecture, Lecture) and course in lecture.courses_attached and course in self.courses_in_progress:
+            if course in lecture.grades:
+                lecture.grades[course] += [grade]
+            else:
+                lecture.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
-class Mentor:
-    def __init__(self, name, surname):
-        self.name = name
-        self.surname = surname
-        self.courses_attached = []
 
+class Reviewer(Mentor):
+    '''класс экспертов, проверяющих домашнее задание'''
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -23,22 +41,23 @@ class Mentor:
         else:
             return 'Ошибка'
 
-class Lecture(Mentor):
-    '''класс лекторов'''
-    pass
-
-class Reviewer(Mentor):
-    '''класс экспертов, проверяющих домашнее задание'''
-    pass
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor = Reviewer('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
 
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 
+best_lecture = Lecture('Ivan', 'Fedorov')
+best_lecture.courses_attached += ['Python']
+
+best_student.rate_lecture(best_lecture, 'Python', 10)
+best_student.rate_lecture(best_lecture, 'Python', 9)
+best_student.rate_lecture(best_lecture, 'Python', 10)
+
 print(best_student.grades)
+print(best_lecture.grades)
